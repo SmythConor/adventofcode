2d87716202f75a1941e59209561cefd98a866aac
Conor Smyth <cnrsmyth@gmail.com>
2017-12-05 23:00:35 +0000

    Updated day 5 to use a function to modify pointer and one common solver function

--- day_5/maze.js
+++ day_5/maze.js
@@ -4,62 +4,54 @@ const input = `${__dirname}/input`;
 part1();
 part2();
 
-function part2() {
-	const lines = utils.readFile(input);
-	lines
-		.then((output) => {
-			const values = output.map(Number);
-			// const values = [0, 3, 0, 1, -3];
+function solve(pointerModifier) {
+	return new Promise((resolve, reject) => {
+		const lines = utils.readFile(input);
+		lines
+			.then((output) => {
+				const values = output.map(Number);
 
-			const upperBound = values.length;
-			let outside = false;
-			let pointer = 0;
-			let stepCounter = 0;
+				const upperBound = values.length;
+				let outside = false;
+				let pointer = 0;
+				let stepCounter = 0;
 
-			while (!outside) {
-				const value = values[pointer];
+				while (!outside) {
+					const value = values[pointer];
 
-				if (value >= 3) {
-					values[pointer]--;
-				} else {
-					values[pointer]++;
+					values[pointer] = pointerModifier(values[pointer]);
+
+					pointer += value;
+					stepCounter++;
+
+					if (pointer >= upperBound) {
+						outside = true;
+					}
 				}
 
-				pointer += value;
-				stepCounter++;
+				resolve(stepCounter);
+			})
+			.catch(reject);
+	});
+}
 
-				if (pointer >= upperBound) {
-					outside = true;
-				}
+function part2(pointerModifier) {
+	let stepCounter = solve((value) => {
+			if (value >= 3) {
+				return value - 1;
+			} else {
+				return value + 1;
 			}
-
+		})
+		.then((stepCounter) => {
 			console.log(`Took ${stepCounter} steps which should equal 26948068`);
 		})
 		.catch(console.log);
 }
 
 function part1() {
-	const lines = utils.readFile(input);
-	lines
-		.then((output) => {
-			const values = output.map(Number);
-			// const values = [0, 3, 0, 1, -3];
-
-			const upperBound = values.length;
-			let outside = false;
-			let pointer = 0;
-			let stepCounter = 0;
-
-			while (!outside) {
-				const value = values[pointer];
-				values[pointer]++;
-
-				pointer += value;
-				stepCounter++;
-				if (pointer >= upperBound) {
-					outside = true;
-				}
-			}
+	let stepCounter = solve((value) => value + 1)
+		.then((stepCounter) => {
 			console.log(`Took ${stepCounter} steps which should equal 355965`);
 		})
 		.catch(console.log);
