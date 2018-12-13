3d575da636e382f751d9f04e300582bce99c67cd
Conor Smyth <cnrsmyth@gmail.com>
2018-12-13 16:49:34 +0000

    Added making of input files and js file to make day
    JS file gets the default requires that every day has

--- make-day.js
+++ make-day.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const day = process.argv[2];
+const programName = `${process.argv[3].split(' ').join('-').toLowerCase()}.js`;
+
+const programDefaults = 'const utils = require(\'../utils/utils\');\nconst inputFile = `${__dirname}/input`;';
 
 if (day) {
 	const dirName = `day_${day}`;
@@ -13,6 +16,26 @@ if (day) {
 			}
 		} else {
 			console.log(`Making directory ${dirName}`);
+
+			const inputFilenames = ['test_input', 'input', programName];
+
+			inputFilenames.forEach(inputFilename => {
+				const output = (() => {
+					if (inputFilename.indexOf('.js') > 0) {
+						return programDefaults;
+					} else {
+						return '';
+					}
+				})();
+
+				fs.writeFile(`${dirName}/${inputFilename}`, output, (err) => {
+					if (err) {
+						console.log(err);
+					} else {
+						console.log(`Making file ${dirName}/${inputFilename}`);
+					}
+				});
+			});
 		}
 	});
 }
